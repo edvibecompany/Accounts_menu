743f218dd060fa3e3595ec6832a955a6c8d03feb
edvibecompany <edvibe.company@gmail.com>
2023-06-15 10:17:33 +0300

    add 1 test

---
+++ test_open_accounts.py
@@ -0,0 +1,21 @@
+from selene import browser, have, be
+
+def test_accounts_check():
+    browser.open('https://preview.edvibe.com/Account/Login')
+    browser.element('.form-input [name=Email]').type('misha-marinov@mail.ru')
+    browser.element('.form-input [name=Password]').type('liveUT00mPE8CB7Z').press_enter()
+    browser.wait_until(browser.element('div.auth-form.auth-container > form > button:nth-child(4)').should(be.visible))
+    browser.element('div.auth-form.auth-container > form > button:nth-child(4)').click()
+    browser.wait_until(have.url('https://edvibe.com/TeacherAccount/lessons'))
+    browser.element('.account-select-container').click()
+    browser.element('div.accounts-list > div:nth-child(2)').click()
+    browser.should(have.url_containing('/TeacherAccount/marathon/marathons'))
+    browser.element('.account-select-container').click()
+    browser.element('div.teacher-account-select-popup.active > div > div.accounts-list > div:nth-child(3)').click()
+    browser.should(have.url_containing('/TeacherAccount/academy/main'))
+    browser.element('.account-select-container').click()
+    browser.element('div.teacher-account-select-popup.active > div > div.accounts-list > div:nth-child(6)').click()
+    browser.should(have.url_containing('/student/lessons'))
+    browser.element('.account-select-container').click()
+    browser.element('div.teacher-account-select-popup.active > div > div.accounts-list > div:nth-child(1)').click()
+    browser.should(have.url_containing('/TeacherAccount/lessons'))
